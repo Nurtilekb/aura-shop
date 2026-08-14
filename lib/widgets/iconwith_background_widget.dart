3673b643762276e25ee4b530cf1ac02b519e3c83
Nurtilek <abdlhtv06@gmail.com>
2026-08-14 19:01:21 +0300

    add that parametr can be use

--- lib/widgets/iconwith_background_widget.dart
+++ lib/widgets/iconwith_background_widget.dart
@@ -1,26 +1,50 @@
+import 'package:aurashop/bloc/theme/theme_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class IconWithBack extends StatelessWidget {
-  const IconWithBack({super.key, this._imagePath, this.sizes, this.padding});
+  const IconWithBack({
+    super.key,
+    this._imagePath,
+    this.sizes,
+    this.padding,
+    this.bordRadius,
+    this.forborder,
+    this.backroundcolor,
+    this._ontap,
+  });
   final String? _imagePath;
   final double? sizes;
   final EdgeInsetsGeometry? padding;
+  final BorderRadiusGeometry? bordRadius;
+  final BoxBorder? forborder;
+  final Color? backroundcolor;
+  final VoidCallback? _ontap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: padding ?? EdgeInsets.all(10),
-      width: sizes ?? 100,
-      height: sizes ?? 100,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25),
-        color: const Color(0xFF5D50FE).withOpacity(0.1),
-        border: Border.all(width: 0, color: Colors.transparent),
-      ),
-      child: Image.asset(
-        _imagePath ?? 'assets/icons/pochta.png',
-        width: 25,
-        height: 25,
-      ),
+    return BlocBuilder<ThemeCubit, ThemeState>(
+      builder: (context, state) {
+        return InkWell(
+          borderRadius: BorderRadius.circular(12),
+          onTap: _ontap,
+          child: Container(
+            padding: padding ?? EdgeInsets.all(10),
+            width: sizes ?? 100,
+            height: sizes ?? 100,
+            decoration: BoxDecoration(
+              borderRadius: bordRadius ?? BorderRadius.circular(25),
+              color: backroundcolor ?? state.directAccentColor,
+              border:
+                  forborder ?? Border.all(width: 0, color: Colors.transparent),
+            ),
+            child: Image.asset(
+              _imagePath ?? 'assets/icons/pochta.png',
+              width: 25,
+              height: 25,
+            ),
+          ),
+        );
+      },
     );
   }
 }
