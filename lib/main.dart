3dce8c11e13a47381664a7401d373c7ae215c604
Nurtilek <abdlhtv06@gmail.com>
2026-08-17 11:13:16 +0300

    added bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:aurashop/bloc/theme/theme_bloc.dart';
 import 'package:aurashop/router/app_router.dart';
+import 'package:aurashop/screens/home_screen.dart';
 import 'package:aurashop/theme/app_colors.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -14,10 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => ThemeCubit(),
+      create: (_) => ThemeCubit(),
       child: BlocBuilder<ThemeCubit, ThemeState>(
         builder: (context, state) {
           final brightness = MediaQuery.platformBrightnessOf(context);
+
           final isDarkMode =
               state.themeMode == ThemeModeStatus.dark ||
               (state.themeMode == ThemeModeStatus.system &&
@@ -28,6 +31,7 @@ class MyApp extends StatelessWidget {
               : AppColors.light(state.seedColor);
 
           return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
             title: 'AuraShop',
             theme: appColors.toThemeData(),
             routerConfig: AppRouter().config(),
@@ -35,5 +39,84 @@ class MyApp extends StatelessWidget {
         },
       ),
     );
+  }
+}
+
+@RoutePage()
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const SizedBox.shrink(),
+    const SizedBox.shrink(),
+    const SizedBox.shrink(),
+    const SizedBox.shrink(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(index: _selectedIndex, children: _screens),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.only(top: 6),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            top: BorderSide(
+              color: Color.fromARGB(137, 158, 158, 158),
+              width: 1,
+            ),
+          ),
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          currentIndex: _selectedIndex,
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.purple.shade700,
+          unselectedItemColor: Colors.grey.shade600,
+          showUnselectedLabels: true,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: 'Главная',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.grid_view),
+              label: 'Каталог',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.delete_outline_outlined),
+              label: 'Корзина',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_border_outlined),
+              label: 'Избранное',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_4_outlined),
+              label: 'Профиль',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _getTitle(int index) {
+    const titles = ['Главная', 'Поиск', 'Корзина', 'Избранное', 'Профиль'];
+    return titles[index];
   }
 }
