4da4cde3bdd88aad0cf8b5f662e8d1389dfb8611
Nurtilek <abdlhtv06@gmail.com>
2026-08-25 17:27:55 +0600

    Add logout confirmation dialog in ProfileScreen

--- lib/features/profile/presentation/screens/profile_screen.dart
+++ lib/features/profile/presentation/screens/profile_screen.dart
@@ -3,6 +3,7 @@ import 'package:aurashop/core/routing/app_router.gr.dart';
 import 'package:aurashop/bloc/auth/auth_bloc.dart';
 import 'package:aurashop/bloc/auth/auth_event.dart';
 import 'package:aurashop/shared/widgets/custom_widgets/pressed_button.dart';
+import 'package:aurashop/shared/widgets/diolog.dart';
 import 'package:aurashop/shared/widgets/profile_widgets/menu_item.dart';
 import 'package:aurashop/shared/widgets/profile_widgets/stat_card_widget.dart';
 import 'package:auto_route/auto_route.dart';
@@ -118,8 +119,21 @@ class ProfileScreen extends StatelessWidget {
                       child: PressedButton(
                         height: 60,
                         onPressed: () {
-                          context.read<AuthBloc>().add(AuthLogoutRequested());
-                          context.router.replaceAll([const LoginRoute()]);
+                          showCustomDialog(
+                            context: context,
+                            type: DialogType.warning,
+                            title: 'Удалить заказ?',
+                            description:
+                                'Вы действительно хотите выйти мз панели? Это действие нельзя будет отменить.',
+                            accentColor: const Color(0xFFE53E3E),
+                            primaryButtonText: 'Да, выйти',
+                            onPrimaryPressed: () {
+                              context.read<AuthBloc>().add(
+                                AuthLogoutRequested(),
+                              );
+                            },
+                            secondaryButtonText: 'Отмена',
+                          );
                         },
                         backgroundColor: Colors.white,
                         borderColor: Colors.grey,
