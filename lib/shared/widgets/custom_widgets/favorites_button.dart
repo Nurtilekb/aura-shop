e766c60ce31bda668de41c77cb994a7e47fdc1e3
Nurtilek <abdlhtv06@gmail.com>
2026-08-26 19:21:28 +0600

    add FavoriteButton widget for managing favorite items

---
+++ lib/shared/widgets/custom_widgets/favorites_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class FavoriteButton extends StatelessWidget {
+  const FavoriteButton({
+    required this.isFavorite,
+    required this.accentColor,
+    required this.onTap,
+  });
+
+  final bool isFavorite;
+  final Color accentColor;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(6),
+        decoration: BoxDecoration(
+          color: Colors.white.withValues(alpha: 0.9),
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.08),
+              blurRadius: 4,
+              offset: const Offset(0, 1),
+            ),
+          ],
+        ),
+        child: Icon(
+          isFavorite ? Icons.favorite : Icons.favorite_border,
+          color: isFavorite ? Colors.red : Colors.grey.shade500,
+          size: 20,
+        ),
+      ),
+    );
+  }
+}
